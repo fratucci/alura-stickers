aeba7edc197f7c6d66969ea2210b87d542de5015
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-03-28 09:18:13 -0300

    Estrelas Notas

--- src/App.java
+++ src/App.java
@@ -24,10 +24,16 @@ public class App {
         System.out.println("\u001b[37m \u001b[44m Alura \u001b[m");
         // exibir dados
         for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println("\u001b[31m \u001b[43m" + filme.get("title") + "\u001b[m");
+            System.out.println("\u001b[1mTítulo:\u001b[m\u001b[31m \u001b[43m" + filme.get("title") + "\u001b[m");
             System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            System.out.print("\u001b[1mNota: \u001b[m" + filme.get("imDbRating") + " ");
+            Integer nota = Integer.valueOf(filme.get("imDbRating").substring(0, 1));
+            for (int i = 0; i <= nota; i++) {
+                System.out.print("⭐");
+            }
             System.out.println();
+            System.out.println();
+
         }
 
     }
