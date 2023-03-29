e58ba213fbfe07b673e772179dcea33c12527310
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-03-29 15:10:56 -0300

    interface classse extrator

--- src/App.java
+++ src/App.java
@@ -1,48 +1,40 @@
 import java.io.File;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
         // String url =
-        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/NASA-APOD.json";
-        URI endereco = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-        // System.out.println(body);
+        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/NASA-APOD.json";
+        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
+        ClienteHttp http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
         // extrair dados qu interessam
-        JsonParser parser = new JsonParser();
-        List<Map<String, String>> listaDeConteudos = parser.parse(body);
 
         File diretorio = new File("saida/");
         diretorio.mkdir();
 
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
         // exibir dados
-        for (Map<String, String> conteudo : listaDeConteudos) {
 
-            String urlImagem = conteudo.get("url");
-            InputStream inputStream = new URL(urlImagem).openStream();
+        for (Conteudo conteudo : conteudos) {
 
-            String titulo = (conteudo.get("title"));
-            String nomeArquivo = diretorio + "/" + titulo.replace(":", "-") + ".png";
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+
+            String nomeArquivo = diretorio + "/" + conteudo.getTitulo().replace(":", "-") + ".png";
 
             GeradoraDeFigurinha geradora = new GeradoraDeFigurinha();
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println("\u001b[1mTítulo:\u001b[m\u001b[31m \u001b[43m" + titulo + "\u001b[m");
+            System.out.println("\u001b[1mTítulo:\u001b[m\u001b[31m \u001b[43m" + conteudo.getTitulo() + "\u001b[m");
             // System.out.println(filme.get("image"));
             // System.out.print("\u001b[1mNota: \u001b[m" + filme.get("imDbRating") + " ");
             // Integer nota = Integer.valueOf(filme.get("imDbRating").substring(0, 1));
--- src/ExtratorDeConteudoDoIMDB.java
+++ src/ExtratorDeConteudoDoIMDB.java
@@ -1,3 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class ExtratorDeConteudoDoIMDB implements ExtratorDeConteudo {
 
+  public List<Conteudo> extraiConteudos(String json) {
+
+    JsonParser parser = new JsonParser();
+    List<Map<String, String>> listaDetAributos = parser.parse(json);
+
+    List<Conteudo> conteudos = new ArrayList<>();
+
+    for (Map<String, String> atributos : listaDetAributos) {
+      String titulo = atributos.get("title");
+      String urlImagem = atributos.get("image");
+
+      Conteudo conteudo = new Conteudo(titulo, urlImagem);
+
+      conteudos.add(conteudo);
+    }
+
+    return conteudos;
+  }
 }
